ccb53fcca634c2c9b190f32597afb5f93a072a88
Ragnar-Oock <ugo.dumontot@gmail.com>
2025-11-01 17:43:26 +0100

    feat: initialise the grid with random cards

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,25 @@ import GridDisplay from "./components/grid-display.vue";
 import HandDisplay from "./components/hand-display.vue";
 import HarvestResources from "./components/harvest-resources.vue";
 import { useDeckStore } from "./stores/deck.store";
+import { useGridStore } from "./stores/grid.store";
+
+const grid = useGridStore();
+
 const deckStore = useDeckStore();
 registerAllCards(deckStore.register);
+
+function initialiseGridAtRandom() {
+	for (let x = 0; x < 10; x++) {
+		for (let y = 0; y < 10; y++) {
+			grid.setCell({
+				position: {x, y},
+				card: deckStore.pick().create(),
+			});
+		}
+	}
+}
+
+initialiseGridAtRandom();
 
 </script>
 
