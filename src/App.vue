b1d781b16d9bd23d3413b524214b906a18cd4120
Ragnar-Oock <ugo.dumontot@gmail.com>
2025-11-08 19:50:08 +0100

    feat: initialize the game with 7 pre-drawn cards

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,39 @@
 <script setup lang="ts">
-import { registerAllCards } from "./cards.data";
-import BoardView from "./components/board-view.vue";
-import DraggedItem from "./components/dragged-item.vue";
-// import AddResourceForm from "./components/add-resource-form.vue";
-// import ConsumeResource from "./components/consume-resource.vue";
-import HandDisplay from "./components/hand-display.vue";
-import HarvestResources from "./components/harvest-resources.vue";
-import ResourcePile from "./components/resource-pile.vue";
-import { iter } from "./helpers/iterator.helper";
-import { useDeckStore } from "./stores/deck.store";
-import { GridVec, useGridStore } from "./stores/grid.store";
+	import { registerAllCards } from "./cards.data";
+	import BoardView from "./components/board-view.vue";
+	import DraggedItem from "./components/dragged-item.vue";
+	// import AddResourceForm from "./components/add-resource-form.vue";
+	// import ConsumeResource from "./components/consume-resource.vue";
+	import HandDisplay from "./components/hand-display.vue";
+	import HarvestResources from "./components/harvest-resources.vue";
+	import ResourcePile from "./components/resource-pile.vue";
+	import { iter } from "./helpers/iterator.helper";
+	import { useDeckStore } from "./stores/deck.store";
+	import { GridVec, useGridStore } from "./stores/grid.store";
 
-const grid = useGridStore();
+	const grid = useGridStore();
 
-const deckStore = useDeckStore();
-registerAllCards(deckStore.register);
+	const deckStore = useDeckStore();
+	registerAllCards(deckStore.register);
 
-function initialiseGridAtRandom() {
-	for (let x = 0; x < 10; x++) {
-		for (let y = 0; y < 10; y++) {
-			grid.setCell({
-				position: {x, y} as GridVec,
-				card: deckStore.pick().create(),
-			});
+	function initialiseGridAtRandom(size: number) {
+		for (let x = 0; x < size; x++) {
+			for (let y = 0; y < size; y++) {
+				grid.setCell({
+					position: {x, y} as GridVec,
+					card: deckStore.pick().create(),
+				});
+			}
 		}
 	}
-}
 
-// initialiseGridAtRandom();
+	function initializeHandAtRandom(amount: number) {
+		iter(amount)
+			.forEach(() => deckStore.draw())
+	}
+
+	// initialiseGridAtRandom(5);
+	initializeHandAtRandom(7);
 
 </script>
 
@@ -45,8 +51,8 @@ function initialiseGridAtRandom() {
 </template>
 
 <style scoped>
-.ui {
-	position: fixed;
-	bottom: 0;
-}
+	.ui {
+		position: fixed;
+		bottom: 0;
+	}
 </style>
