148094c7ba6b34de66a29f25007dc740768ab06b
Ragnar-Oock <ugo.dumontot@gmail.com>
2025-11-08 19:27:59 +0100

    fixup! refactor: explode grid-display in multiple components and stores
    missing subtractVec and setVec helpers

--- src/helpers/vector.helper.ts
+++ src/helpers/vector.helper.ts
@@ -11,10 +11,21 @@ export interface Vector2 {
 
 export type ReadonlyVector2 = Readonly<Vector2>;
 
-export const addVec = <vec extends Vector2>(a: vec, b: vec): vec => ({
+export const addVec = <vec extends Vector2>(a: Readonly<vec>, b: Readonly<vec>): vec => ({
 	x: a.x + b.x,
 	y: a.y + b.y,
 } as vec);
+
+export const subtractVec = <vec extends Vector2>(a: Readonly<vec>, b: Readonly<vec>): vec => ({
+	x: a.x - b.x,
+	y: a.y - b.y,
+} as vec);
+
+export const setVec = <vec extends Vector2>(vector: vec, to: Readonly<vec>): vec => {
+	vector.x = to.x;
+	vector.y = to.y;
+	return vector;
+}
 
 export const inverseVec = <vec extends Vector2>({x, y}: vec): vec => ({
 	x: -x,
