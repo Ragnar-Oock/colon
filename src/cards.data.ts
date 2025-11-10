1faaad496a2b9a364c4d8705932ff86959ebc228
Ragnar-Oock <ugo.dumontot@gmail.com>
2025-11-11 00:07:39 +0100

    predicates

--- src/cards.data.ts
+++ src/cards.data.ts
@@ -1,55 +1,13 @@
+import { combine, expectAtLeast, expectNone } from "./helpers/neighborhood-predicate.helper";
+import { countType } from "./helpers/score-multiplier.helper";
+import { ofType, sameType } from "./helpers/score-predicate";
 import { pick } from "./math.helper";
-import { CardDescriptor, CardInstance } from "./stores/deck.store";
-import { Cell } from "./stores/grid.store";
-import { card, ResourceCard, resourceTriggers } from "./stores/resource.store";
-
-export type NeighborhoodPredicate = (neighborhood: Cell[]) => boolean;
+import { card, CardDescriptor, CardInstance } from "./stores/deck.store";
+import { FilledCell } from "./stores/grid.store";
+import { ResourceCard, resourceTriggers } from "./stores/resource.store";
 
 export type CardType = CardInstance["name"];
 
-/**
- * Check that at least one of the neighbors is of a required set of types
- * @param oneOf a set of types, at least one of which should be present at least once in the future neighbors
- * @returns is the neighborhood suitable ?
- */
-const expectAtLeast = (...oneOf: CardType[]): NeighborhoodPredicate =>
-	neighborhood =>
-		neighborhood.some(neighbor => oneOf.includes(neighbor.card?.name!));
-
-/**
- * Check that none of the neighbors is of a given set of types
- * @param of a set of types, none of which should be present in the future neighbors
- * @returns is the neighborhood suitable ?
- */
-const expectNone = (...of: CardType[]): NeighborhoodPredicate => neighborhood =>
-	!neighborhood.some(neighbor => of.includes(neighbor.card?.name!))
-
-/**
- * Check that at least one of the neighbors is of a required set of type. Types are counted as a whole, to count each
- * type individually use multiple instances of {@link expectAtMost `expectAtMost()`} with {@link combine `combine()`}.
- * @param amount how many neighbors to expect
- * @param of a set of types that should be present at least `amount` times
- * @returns is the neighborhood suitable ?
- */
-const expectAtMost = (amount: number, ...of: CardType[]): NeighborhoodPredicate =>
-	neighborhood => neighborhood.reduce(
-		(count, neighbor) => count + (of.includes(neighbor.card?.name!) ? 1 : 0),
-		0
-	) > amount;
-
-/**
- * Combine multiple {@link NeighborhoodPredicate} into a single one, all of which should match.
- * @param predicates
- */
-const combine = (...predicates: NeighborhoodPredicate[]): NeighborhoodPredicate =>
-	neighborhood => predicates.every(predicate => predicate(neighborhood));
-
-/**
- * Combine multiple {@link NeighborhoodPredicate} into a single one, at least one of which should match.
- * @param predicates
- */
-const either = (...predicates: NeighborhoodPredicate[]): NeighborhoodPredicate =>
-	neighborhood => predicates.some(predicate => predicate(neighborhood));
 
 export const cards = [
 	{
