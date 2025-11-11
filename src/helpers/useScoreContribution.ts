10b447a9a4fa11ca811ea14568c54c924244bda9
Ragnar-Oock <ugo.dumontot@gmail.com>
2025-11-11 11:27:42 +0100

    fix: score preview not showing up

--- src/helpers/useScoreContribution.ts
+++ src/helpers/useScoreContribution.ts
@@ -1,4 +1,5 @@
 import { computed, ComputedRef, MaybeRefOrGetter, toValue } from "vue";
+import { useBoardStore } from "../stores/board.store";
 import { useDeckStore } from "../stores/deck.store";
 import { GridVec, useGridStore } from "../stores/grid.store";
 import { equalsVec } from "./vector.helper";
@@ -7,15 +8,17 @@ export function useScoreContribution(at: MaybeRefOrGetter<GridVec>): ComputedRef
 	const position = toValue(at);
 	const grid = useGridStore();
 	const deck = useDeckStore()
+	const board = useBoardStore();
 
 	return computed(() => {
 		const card = deck.active
+		const placement = board.hoveredCell;
 
-		if (card === null) {
+		if (card === null || placement === null || !grid.canPlace(card, placement)) {
 			return null;
 		}
 		return grid
-				.getScoreContributors(card, position, Array.from(grid.cells).concat([{card, position}]))
+				.getScoreContributors(card, placement, Array.from(grid.cells).concat([{card, position: placement}]))
 				.find((cell) => equalsVec(position, cell.position))
 				?.score
 			?? null;
