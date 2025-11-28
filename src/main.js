69c63d3032f6e2831dafb5f46d90f9547ae52997
Ragnar-Oock <ugo.dumontot@gmail.com>
2025-11-28 23:42:53 +0100

    feat: create display contents style utility

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-import './assets/main.css'
+import './assets/main.scss'
 
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
 import {createPinia} from "pinia";
 
 createApp(App)
-    .use(createPinia())
-    .mount('#app')
+	.use(createPinia())
+	.mount('#app')
