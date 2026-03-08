7b62d0bd322cc1c4471504bd5678fddef26b0634
Ragnar-Oock <ugo.dumontot@gmail.com>
2026-03-08 17:35:15 +0100

    fix(xoram): add missing dependency

--- src/plugins/board/board.plugin.ts
+++ src/plugins/board/board.plugin.ts
@@ -1,11 +1,18 @@
 import { definePlugin, dependsOn } from "@xoram/core";
-import { addChild, defineComponentDefinition, register, rootHarness } from "@xoram/plugin-panoramique";
+import {
+	addChild,
+	defineComponentDefinition,
+	panoramiquePlugin,
+	register,
+	rootHarness
+} from "@xoram/plugin-panoramique";
 import { gridPlugin } from "../grid/grid.plugin";
 import BoardView from "./board-view.vue";
 
 const board = defineComponentDefinition('board', BoardView);
 export const boardPlugin = definePlugin('board', () => {
 	dependsOn(gridPlugin);
+	dependsOn(panoramiquePlugin);
 	register(board);
 	addChild(rootHarness, board.id)
 })
